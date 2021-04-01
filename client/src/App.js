d01c2ef585e88bf5716fb93e9e126c4d93b876bd
apotato369550 <johnandresyap510@gmail.com>
2021-04-01 13:30:03 +0800

    Add CORS, and express.json and SQL

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import React, { useState } from 'react';
+import Axios from 'axios';
 
-// continue here
+
+// this won't post properly
 function App() {
   const [name, setName] = useState('');
   const [age, setAge] = useState(0);
@@ -9,7 +11,20 @@ function App() {
   const [position, setPosition] = useState('');
   const [wage, setWage] = useState(0);
 
-  // create display data function that c-logs state info out
+  const addEmployee = () => {
+    // twas lacking /create
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage
+    }).then(() => {
+      console.log("success");
+    });
+  }
+
+  // learn about javascript promises
   return (
     <div className="App">
       <div className="information">
@@ -43,7 +58,11 @@ function App() {
           onChange={(event) => setWage(event.target.value)}
         />
 
-        <button>Add Employee</button>
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      <hr />
+      <div className="employees">
+        <button>Show Employees</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const mysql = require("mysql")
+const cors = require("cors")
+
+// had to install cors library thingy
+// the thing won't post properly, might be because a route has not been set properly here
+// change /create to /
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
     user: "root",
@@ -9,13 +16,30 @@ const db = mysql.createConnection({
     database: "crud_employee_app"
 })
 
+// continue tutorial
 app.post("/create", (req, res) => {
-    // continue here
-    // learn about express requests
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query(
+        "INSERT INTO employees (name, age, country, position, wage) VALUES (?, ?, ?, ?, ?)",
+        [name, age, country, position, wage],
+        (err, result) => {
+            if(err){
+                console.log(err);
+            } else {
+                // put req instead of res.send()
+                res.send("Values Inserted");
+            }
+        }
+    )
 })
 
 app.listen(3001, () => {
     console.log("Your server is running on port 3001")
 })
 
-// figure out mysql workbench
+// work on this now
